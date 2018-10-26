1ca149697837c811c52328a8919f74ba715038df
CIECs20171103196 <1614211282@qq.com>
2018-10-26 20:34:09 +0800

    Initial Commit

---
+++ qewqwe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qewqwe
+//
+//  Created by s20171103196 on 2018/10/26.
+//  Copyright © 2018 s20171103196. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ qewqweTests/qewqweTests.swift
@@ -0,0 +1,34 @@
+//
+//  qewqweTests.swift
+//  qewqweTests
+//
+//  Created by s20171103196 on 2018/10/26.
+//  Copyright © 2018 s20171103196. All rights reserved.
+//
+
+import XCTest
+@testable import qewqwe
+
+class qewqweTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
